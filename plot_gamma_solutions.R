efaa8e1940f32f8ca09d8e97f675574b2ef2162d
pruzanov <pruzanov@oicr.on.ca>
2020-10-15 18:07:23 -0400

    Updated summary plot producing script

--- plot_gamma_solutions.R
+++ plot_gamma_solutions.R
@@ -8,9 +8,9 @@ option_list = list(
               help="json file with gamma solutions from sequenza", metavar="character"),
   make_option(c("-o", "--outputFile"), type="character", default=NULL,
               help="output png file", metavar="character"),
-  make_option(c("-w", "--width"), type="integer", default="1440",
+  make_option(c("-w", "--width"), type="integer", default="1200",
               help="width of png file", metavar="integer"),
-  make_option(c("-h", "--height"), type="integer", default="440",
+  make_option(c("-h", "--height"), type="integer", default="400",
               help="height of png file", metavar="integer"))
 
 opt_parser = OptionParser(option_list = option_list, add_help_option = FALSE)
@@ -24,14 +24,19 @@ df.cellularity<-data.frame(gamma = gamma.solutions$gamma, value = gamma.solution
 df.ploidy<-data.frame(gamma = gamma.solutions$gamma, value = gamma.solutions$ploidy, type="ploidy")
 df.segmentation<-data.frame(gamma = gamma.solutions$gamma, value=gamma.solutions$no_segments, type="no. segments")
 df<-rbind(df.segmentation, df.cellularity, df.ploidy)
+
+df$type_f<-factor(df$type, levels = c('no. segments', 'cellularity','ploidy')) # determines panel order
+df<-data.frame(df, y_min=0)
+
 p<-ggplot(df, aes(x = gamma, y = value))+
   geom_line(linetype = "dashed", aes(colour = type, group = type), size = 1)+
-  geom_point(aes(colour = type, group = type),size = 4)+
+  geom_point(aes(colour = type, group = type_f),size = 4)+
   scale_x_continuous(breaks = as.numeric(gamma.solutions$gamma)) +
   facet_grid(rows = vars(type), scales="free")+
   theme_bw(base_size = 18)+
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  geom_blank(aes(y = y_min)) # trick to set y-axis scale origin at the zero value
 
 png(filename = file.path(outFile), type="cairo", width = opt$width, height = opt$height)
-print(p)   
-dev.off() 
+print(p)
+dev.off()
